da37b21d2f048e59c651c374ff5852d733634368
alrudo <alrudo@ttu.ee>
2021-05-28 15:45:56 +0300

    Did some refactoring, removed unused code, files and commented files.
    Achieved with help of Artur.

--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -1,14 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpEvent } from '@angular/common/http';
-// import { Credentials, CredentialsService } from './credentials.service';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-export interface LoginContext {
-  username: string;
-  password: string;
-  remember?: boolean;
-}
 
 /**
  * Provides a base for authentication workflow.
@@ -22,22 +14,7 @@ export class AuthenticationService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {} // , private credentialsService: CredentialsService) {}
-
-  /**
-   * Authenticates the user.
-   * @param context The login parameters.
-   * @return The user credentials.
-   */
-  // login(context: LoginContext): Observable<Credentials> {
-  //   // Replace by proper authentication call
-  //   const data = {
-  //     username: context.username,
-  //     token: '123456',
-  //   };
-  //   this.credentialsService.setCredentials(data, context.remember);
-  //   return of(data);
-  // }
+  constructor(private http: HttpClient) {}
 
   /**
    * Logs out the user and clear credentials.
